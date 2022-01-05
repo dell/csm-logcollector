4eb846782f73660ef6cc4b52075d0bb8357b856c
srinivas-rao5 <srinivas_rao5@dell.com>
2022-01-05 15:03:50 +0530

    added unit tests for get logs

--- csm/csm_test.go
+++ csm/csm_test.go
@@ -5,7 +5,7 @@ import (
 	"io/ioutil"
 	"strings"
 	"testing"
-
+	"os"
 	"github.com/google/go-cmp/cmp"
 	coordinationv1 "k8s.io/api/coordination/v1"
 	v1 "k8s.io/api/core/v1"
@@ -334,11 +334,11 @@ func TestGetNamespaces(t *testing.T) {
 func TestGetNodes(t *testing.T) {
 	type tests = []struct {
 		description        string
-		expectedNamespaces []string
+		expectedNodes []string
 	}
 
 	var stTests = tests{
-		{"get namespaces", []string{"10.xx.xxx.xxx", "11.xx.xxx.xxx"}},
+		{"get nodes", []string{"10.xx.xxx.xxx", "11.xx.xxx.xxx"}},
 	}
 
 	for _, test := range stTests {
@@ -347,10 +347,153 @@ func TestGetNodes(t *testing.T) {
 			_ = CreateNodes(clientset, "10.xx.xxx.xxx")
 			_ = CreateNodes(clientset, "11.xx.xxx.xxx")
 			gotNodes := GetNodes()
-			if diff := cmp.Diff(gotNodes, test.expectedNamespaces); diff != "" {
-				t.Errorf("%T differ (-got, +want): %s", test.expectedNamespaces, diff)
+			if diff := cmp.Diff(gotNodes, test.expectedNodes); diff != "" {
+				t.Errorf("%T differ (-got, +want): %s", test.expectedNodes, diff)
 				return
 			}
 		})
 	}
+}
+
+func TestGetLogs(t *testing.T){
+	type tests = []struct {
+		description string
+		namespace string
+		podName string
+		leaseName string
+		expected string
+	}
+	current_path , _ := os.Getwd()
+	files , _ := ioutil.ReadDir(current_path)
+	var tar_file_found bool
+	var pflx PowerFlexStruct
+	var pflxTests = tests{
+		{"get logs of powerflex", "csi-powerflex", "pod1", "lease1", "tar file should be created"},
+	}
+	for _, test := range pflxTests {
+		t.Run(test.description, func(t *testing.T) {
+			clientset = fake.NewSimpleClientset()
+			_ = CreateNodes(clientset, "10.xx.xxx.xxx")
+			_ = CreateNodes(clientset, "11.xx.xxx.xxx")
+			_ = CreateNamespace(clientset, "csi-powerflex")
+			_ = CreateNamespace(clientset, "ns-2")
+			_ = CreatePod(clientset, test.namespace, test.podName, "attacher")
+			_ = CreateLease(clientset, test.leaseName, test.namespace, test.podName)
+			pflx.GetLogs(test.namespace, "true")
+			for _, file := range files {
+				if strings.Contains(file.Name(), test.namespace){
+					tar_file_found = true
+				}
+			}
+			if !tar_file_found{
+				t.Errorf("tar creation not sucessfull.")
+			}
+			tar_file_found = false
+		})
+	}
+
+	var pmax PowerMaxStruct
+	var pmaxTests = tests{
+		{"get logs of powermax", "csi-powermax", "pod1", "lease1", "tar file should be created"},
+	}
+	for _, test := range pmaxTests {
+		t.Run(test.description, func(t *testing.T) {
+			clientset = fake.NewSimpleClientset()
+			_ = CreateNodes(clientset, "10.xx.xxx.xxx")
+			_ = CreateNodes(clientset, "11.xx.xxx.xxx")
+			_ = CreateNamespace(clientset, "csi-powermax")
+			_ = CreateNamespace(clientset, "ns-2")
+			_ = CreatePod(clientset, test.namespace, test.podName, "attacher")
+			_ = CreateLease(clientset, test.leaseName, test.namespace, test.podName)
+			pmax.GetLogs(test.namespace, "true")
+			for _, file := range files {
+				if strings.Contains(file.Name(), test.namespace){
+					tar_file_found = true
+				}
+			}
+			if !tar_file_found{
+				t.Errorf("tar creation not sucessfull.")
+			}
+			tar_file_found = false
+		})
+	}
+
+	var pstore PowerStoreStruct
+	var pstoreTests = tests{
+		{"get logs of powerstore", "csi-powerstore", "pod1", "lease1", "tar file should be created"},
+	}
+	for _, test := range pstoreTests {
+		t.Run(test.description, func(t *testing.T) {
+			clientset = fake.NewSimpleClientset()
+			_ = CreateNodes(clientset, "10.xx.xxx.xxx")
+			_ = CreateNodes(clientset, "11.xx.xxx.xxx")
+			_ = CreateNamespace(clientset, "csi-powerstore")
+			_ = CreateNamespace(clientset, "ns-2")
+			_ = CreatePod(clientset, test.namespace, test.podName, "attacher")
+			_ = CreateLease(clientset, test.leaseName, test.namespace, test.podName)
+			pstore.GetLogs(test.namespace, "true")
+			for _, file := range files {
+				if strings.Contains(file.Name(), test.namespace){
+					tar_file_found = true
+				}
+			}
+			if !tar_file_found{
+				t.Errorf("tar creation not sucessfull.")
+			}
+			tar_file_found = false
+		})
+	}
+
+	var pscale PowerScaleStruct
+	var pscaleTests = tests{
+		{"get logs of powerscale", "csi-powerscale", "pod1", "lease1", "tar file should be created"},
+	}
+	for _, test := range pscaleTests {
+		t.Run(test.description, func(t *testing.T) {
+			clientset = fake.NewSimpleClientset()
+			_ = CreateNodes(clientset, "10.xx.xxx.xxx")
+			_ = CreateNodes(clientset, "11.xx.xxx.xxx")
+			_ = CreateNamespace(clientset, "csi-powerscale")
+			_ = CreateNamespace(clientset, "ns-2")
+			_ = CreatePod(clientset, test.namespace, test.podName, "attacher")
+			_ = CreateLease(clientset, test.leaseName, test.namespace, test.podName)
+			pscale.GetLogs(test.namespace, "true")
+			for _, file := range files {
+				if strings.Contains(file.Name(),test.namespace){
+					tar_file_found = true
+				}
+			}
+			if !tar_file_found{
+				t.Errorf("tar creation not sucessfull.")
+			}
+			tar_file_found = false
+		})
+	}
+	
+	var unity UnityStruct
+	var unityTests = tests{
+		{"get logs of unity", "csi-unity", "pod1", "lease1", "tar file should be created"},
+	}
+	for _, test := range unityTests {
+		t.Run(test.description, func(t *testing.T) {
+			clientset = fake.NewSimpleClientset()
+			_ = CreateNodes(clientset, "10.xx.xxx.xxx")
+			_ = CreateNodes(clientset, "11.xx.xxx.xxx")
+			_ = CreateNamespace(clientset, "csi-unity")
+			_ = CreateNamespace(clientset, "ns-2")
+			_ = CreatePod(clientset, test.namespace, test.podName, "attacher")
+			_ = CreateLease(clientset, test.leaseName, test.namespace, test.podName)
+			unity.GetLogs(test.namespace, "true")
+			for _, file := range files {
+				if strings.Contains(file.Name(), test.namespace){
+					tar_file_found = true
+				}
+			}
+			if !tar_file_found{
+				t.Errorf("tar creation not sucessfull.")
+			}
+			tar_file_found = false
+		})
+	}
+
 }
