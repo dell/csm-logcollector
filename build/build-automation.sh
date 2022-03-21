f25f8b7d47f8d3c85f25e47c7fec0cbb97791e0c
kharer5 <Rajshree.Khare@dell.com>
2022-03-21 16:39:21 +0530

    release related changes

---
+++ build/build-automation.sh
@@ -0,0 +1,27 @@
+export PATH=$PATH:/usr/local/go/bin
+rm -rf "$WORKSPACE/build"
+mkdir "$WORKSPACE/build"
+go mod tidy
+# application build version
+version="v0.0.${BUILD_NUMBER}"
+cp config.yml "$WORKSPACE/build/."
+cp README.md "$WORKSPACE/build/."
+go build -o "$WORKSPACE/build/csm-logcollector"
+# scp config and docker files to build agent's Workspace
+sshpass -p "dangerous" scp -o StrictHostKeyChecking=no root@10.247.66.65:/home/akash/golang_app/Dockerfile $WORKSPACE
+sshpass -p "dangerous" scp -o StrictHostKeyChecking=no root@10.247.66.65:/home/akash/golang_app/build/config.yml $WORKSPACE
+images=$(docker images -a | grep "csm-logcollector" | awk '{print $3}')
+if [ -z != $images ]
+then
+docker rmi $images -f
+fi
+docker build -t csm-logcollector .
+# Quay docker registry
+docker tag csm-logcollector quay.io/arindam_datta/csm-logcollector:"${version}"
+docker tag csm-logcollector quay.io/arindam_datta/csm-logcollector
+docker push quay.io/arindam_datta/csm-logcollector
+docker push quay.io/arindam_datta/csm-logcollector:"${version}"
+docker pull quay.io/arindam_datta/csm-logcollector:"${version}"
+# dellemc docker registry
+docker tag csm-logcollector dellemc/csm-log-collector:"${version}"
+docker push dellemc/csm-log-collector:"${version}"
